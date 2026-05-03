15ce1b94f047e5dd0fb74bc20efc21b80a6b7199
MrFoxyen <zeljohn777@gmail.com>
2026-05-04 01:19:28 +0300

    Add files via upload

---
+++ src/main/java/com/example/dictionary/CursovaDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursovaDictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursovaDictionaryApplication.class, args);
+    }
+
+}
